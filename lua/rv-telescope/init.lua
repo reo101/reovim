db67df30b71fa0735541e739e7e418168f72f33e
Pavel Atanasov <pavel.atanasov2001@gmail.com>
2021-07-06 21:54:20 +0300

    Add lots of new config...
    Make dependency and sequencing trees for telescope and treesitter
    Add latex symbols source for compe (WIP)
    Add treesitter-textobjects config
    Add utils.lua helper (WIP, not used yet)

--- lua/rv-telescope/init.lua
+++ lua/rv-telescope/init.lua
@@ -3,48 +3,48 @@ local M = {}
 M.config = function()
 
     local opt = {
-        defaults = {
-            vimgrep_arguments = {
-                'rg',
-                '--color=never',
-                '--no-heading',
-                '--with-filename',
-                '--line-number',
-                '--column',
-                '--smart-case'
-            },
-            prompt_prefix = "> ",
-            selection_caret = "> ",
-            entry_prefix = "  ",
-            initial_mode = "insert",
-            selection_strategy = "reset",
-            sorting_strategy = "descending",
-            layout_strategy = "horizontal",
-            layout_config = {
-                horizontal = {
-                    mirror = false,
-                },
-                vertical = {
-                    mirror = false,
-                },
-            },
-            file_sorter =  require'telescope.sorters'.get_fuzzy_file,
-            file_ignore_patterns = {},
-            generic_sorter =  require'telescope.sorters'.get_generic_fuzzy_sorter,
-            shorten_path = true,
-            winblend = 0,
-            border = {},
-            borderchars = { '─', '│', '─', '│', '╭', '╮', '╯', '╰' },
-            color_devicons = true,
-            use_less = true,
-            set_env = { ['COLORTERM'] = 'truecolor' }, -- default = nil,
-            file_previewer = require'telescope.previewers'.vim_buffer_cat.new,
-            grep_previewer = require'telescope.previewers'.vim_buffer_vimgrep.new,
-            qflist_previewer = require'telescope.previewers'.vim_buffer_qflist.new,
+        -- defaults = {
+            -- vimgrep_arguments = {
+                -- 'rg',
+                -- '--color=never',
+                -- '--no-heading',
+                -- '--with-filename',
+                -- '--line-number',
+                -- '--column',
+                -- '--smart-case'
+            -- },
+            -- prompt_prefix = "> ",
+            -- selection_caret = "> ",
+            -- entry_prefix = "  ",
+            -- initial_mode = "insert",
+            -- selection_strategy = "reset",
+            -- sorting_strategy = "descending",
+            -- layout_strategy = "horizontal",
+            -- layout_config = {
+            --     horizontal = {
+            --         mirror = false,
+            --     },
+            --     vertical = {
+            --         mirror = false,
+            --     },
+            -- },
+            -- file_sorter =  require'telescope.sorters'.get_fuzzy_file,
+            -- file_ignore_patterns = {},
+            -- generic_sorter =  require'telescope.sorters'.get_generic_fuzzy_sorter,
+            -- shorten_path = true,
+            -- winblend = 0,
+            -- border = {},
+            -- borderchars = { '─', '│', '─', '│', '╭', '╮', '╯', '╰' },
+            -- color_devicons = true,
+            -- use_less = true,
+            -- set_env = { ['COLORTERM'] = 'truecolor' }, -- default = nil,
+            -- file_previewer = require'telescope.previewers'.vim_buffer_cat.new,
+            -- grep_previewer = require'telescope.previewers'.vim_buffer_vimgrep.new,
+            -- qflist_previewer = require'telescope.previewers'.vim_buffer_qflist.new,
 
-            -- Developer configurations: Not meant for general override
-            buffer_previewer_maker = require'telescope.previewers'.buffer_previewer_maker
-        },
+            -- -- Developer configurations: Not meant for general override
+            -- buffer_previewer_maker = require'telescope.previewers'.buffer_previewer_maker
+        -- },
         extensions = {
             fzf = {
                 fuzzy = true,                    -- false will only do exact matching
@@ -56,8 +56,8 @@ M.config = function()
             media_files = {
                 -- filetypes whitelist
                 -- defaults to {"png", "jpg", "mp4", "webm", "pdf"}
-                filetypes = {"png", "webp", "jpg", "jpeg"},
-                find_cmd = "rg" -- find command (defaults to `fd`)
+                filetypes = {},
+                -- find_cmd = "rg" -- find command (defaults to `fd`)
             },
         },
     }
--- lua/rv-treesitter/init.lua
+++ lua/rv-treesitter/init.lua
@@ -3,7 +3,7 @@ local M = {}
 M.config = function()
     
     opt = {
-        ensure_installed = "all",
+        ensure_installed = {"cpp", "lua", "javascript", "java", "php", "python", "comment"},
         highlight = {
             enable = true,
         },
@@ -12,6 +12,88 @@ M.config = function()
         },
         rainbow = {
             enable = true,
+        },
+        incremental_selection = {
+            enable = true,
+            keymaps = {
+                init_selection = 'gnn',
+                node_incremental = ".",
+                scope_incremental = "grc",
+                node_decremental = ",",
+            }
+        },
+        refactor = {
+            highlight_definitions = {
+                enable = true
+            },
+            smart_rename = {
+                enable = true,
+                keymaps = {
+                    smart_rename = "grr"
+                }
+            },
+            navigation = {
+                enable = true,
+                keymaps = {
+                    goto_definition = "gnd",
+                    list_definitions = "gnD"
+                }
+            }
+        },
+        textobjects = {
+            enable = true,
+            disable = {},
+            select = {
+                enable = true,
+                keymaps = {
+                    ["af"] = "@function.outer",
+                    ["if"] = "@function.inner",
+                    ["aC"] = "@class.outer",
+                    ["iC"] = "@class.inner",
+                    ["ac"] = "@conditional.outer",
+                    ["ic"] = "@conditional.inner",
+                    ["ae"] = "@block.outer",
+                    ["ie"] = "@block.inner",
+                    ["al"] = "@loop.outer",
+                    ["il"] = "@loop.inner",
+                    ["is"] = "@statement.inner",
+                    ["as"] = "@statement.outer",
+                    ["ad"] = "@comment.outer",
+                    ["am"] = "@call.outer",
+                    ["im"] = "@call.inner"
+                }
+            },
+            swap = {
+                enable = true,
+                swap_next = {
+                    ["<leader>a"] = "@parameter.inner",
+                },
+                swap_previous = {
+                    ["<leader>A"] = "@parameter.inner",
+                },
+            },
+            move = {
+                enable = true,
+                goto_next_start = {
+                    ["]m"] = "@function.outer",
+                    ["]]"] = "@class.outer",
+                },
+                goto_next_end = {
+                    ["]M"] = "@function.outer",
+                    ["]["] = "@class.outer",
+                },
+                goto_previous_start = {
+                    ["[m"] = "@function.outer",
+                    ["[["] = "@class.outer",
+                },
+                goto_previous_end = {
+                    ["[M"] = "@function.outer",
+                    ["[]"] = "@class.outer",
+                },
+            },
+            lsp_interop = {
+                enable = false,
+            }
         },
     }
 
