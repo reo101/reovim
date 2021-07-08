04fb5afbf34fabca4165ee487ac5cccbee5ed1ec
reo101 <pavel.atanasov2001@gmail.com>
2021-07-08 21:02:24 +0300

    Disable auto restoring of sessions

--- lua/rv-autosession/init.lua
+++ lua/rv-autosession/init.lua
@@ -8,7 +8,7 @@ M.config = function()
         auto_session_root_dir = vim.fn.stdpath('data').."/sessions/",
         auto_session_enabled = true,
         auto_save_enabled = true,
-        auto_restore_enabled = true,
+        auto_restore_enabled = false,
         auto_session_suppress_dirs = nil
     }
 
