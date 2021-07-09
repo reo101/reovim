877ec8221779470faa4fcfeab4026af70b81a9e3
Pavel Atanasov <pavel.atanasov2001@gmail.com>
2021-07-09 18:25:03 +0300

    Add hop which-key keybinds...
    <leader>h1 - Hop 1 (Char)
    <leader>h2 - Hop 2 (Chars)
    <leader>hl - Hop Line
    <leader>hw - Hop Word
    <leader>h/ - Hop Pattern

--- lua/rv-hop/init.lua
+++ lua/rv-hop/init.lua
@@ -9,6 +9,21 @@ M.config = function()
 
     require("hop").setup(opt)
 
+    local wk = require("which-key")
+
+    local mappings = {
+        h = {
+            name = "Hop",
+            ["1"] = { function() require("hop").hint_char1() end, "1 Char" },
+            ["2"] = { function() require("hop").hint_char2() end, "2 Chars" },
+            ["l"] = { function() require("hop").hint_lines_skip_whitespace() end, "Line (no whitespace)" },
+            ["w"] = { function() require("hop").hint_words() end, "Words" },
+            ["/"] = { function() require("hop").hint_patterns() end, "Pattern" },
+        },
+    }
+    
+    wk.register(mappings, { prefix = "<leader>" })
+
 end
 
 return M
