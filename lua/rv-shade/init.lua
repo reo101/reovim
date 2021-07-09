b3f92972baf632b5a3a38395b6e71c530b9b1720
Pavel Atanasov <pavel.atanasov2001@gmail.com>
2021-07-09 17:29:53 +0300

    Add shade which-key keybinds...
    <leader>tsu - Toggle Shade (Brightness) Up
    <leader>tsd - Toggle Shade (Brightness) Down
    <leader>tso - Toggle Shade On/Off

--- lua/rv-shade/init.lua
+++ lua/rv-shade/init.lua
@@ -4,15 +4,31 @@ M.config = function()
 
     local opt = {
         overlay_opacity = 50,
-        opacity_step = 1,
-        keys = {
-            brightness_up    = "<C-Up>",
-            brightness_down  = "<C-Down>",
-            toggle           = "<Leader>s",
-        }
+        opacity_step = 10,
+        --[[ keys = {
+            brightness_up    = "<Leader>tsu",
+            brightness_down  = "<Leader>tsd",
+            toggle           = "<Leader>tsa",
+        } ]]
     }
 
     require("shade").setup(opt)
+
+    local wk = require("which-key")
+
+    local mappings = {
+        t = {
+            name = "Toggle",
+            s = {
+                name = "Shade",
+                o = { function() require("shade").toggle() end, "On/Off" },
+                u = { function() require("shade").brightness_up() end, "Brightness up +10" },
+                d = { function() require("shade").brightness_down() end, "Brightness down -10" },
+            },
+        },
+    }
+
+    wk.register(mappings, { prefix = "<leader>" })
 
 end
 
