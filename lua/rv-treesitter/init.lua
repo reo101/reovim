0c2382379bd30548dae07607e5405c4bf711f8e3
reo101 <pavel.atanasov2001@gmail.com>
2021-07-12 21:21:21 +0300

    Add treesitter playground config and toggle...
    <leader>tsg - Toggle TreeSitter PlayGround

--- lua/rv-treesitter/init.lua
+++ lua/rv-treesitter/init.lua
@@ -109,6 +109,24 @@ M.config = function()
         autotag = {
             enable = true,
         },
+        playground = {
+            enable = true,
+            disable = {},
+            updatetime = 25, -- Debounced time for highlighting nodes in the playground from source code
+            persist_queries = false, -- Whether the query persists across vim sessions
+            keybindings = {
+                toggle_query_editor = 'o',
+                toggle_hl_groups = 'i',
+                toggle_injected_languages = 't',
+                toggle_anonymous_nodes = 'a',
+                toggle_language_display = 'I',
+                focus_language = 'f',
+                unfocus_language = 'F',
+                update = 'R',
+                goto_node = '<cr>',
+                show_help = '?',
+            },
+        },
     }
 
     require("nvim-treesitter.configs").setup(opt)
@@ -146,6 +164,7 @@ M.config = function()
                 r = { "<Cmd>TSBufToggle rainbow<CR>", "Rainbow Parenthesis" },
                 p = { "<Cmd>TSBufToggle autopairs<CR>", "Autopairs" },
                 t = { "<Cmd>TSBufToggle autotag<CR>", "Autotags" },
+                g = { "<Cmd>TSPlaygroundToggle<CR>", "PlayGround" },
             },
         },
     }
