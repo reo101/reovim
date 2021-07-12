252c17c414188dc5acf83e98dfdfd52fb433499a
Pavel Atanasov <pavel.atanasov2001@gmail.com>
2021-07-13 01:00:02 +0300

    Add lsp which-key keybinds...
    <leader>l - LSP
    .g - Go
    gd/..d - Definition
    gD/..D - Decaration
    gy/..y - Type Definition
    gr/..r - References
    gi/..i - Implementation
    .d - Diagnostics
    ..n - Next
    ..p - Previous
    ..l - Line Diagnostics
    ..q - Send to loclist
    .r - Rename
    .a - Code Action
    .f - Format
    .w - Workspace
    ..a - Add (Folder)
    ..r - Remove (Folder)
    ..l - List (Folders)
    [d - Previous Diagnostic
    ]d - Next Diagnostic

--- lua/rv-lsp/init.lua
+++ lua/rv-lsp/init.lua
@@ -5,33 +5,61 @@ local nvim_lsp = require("lspconfig")
 -- Use an on_attach function to only map the following keys
 -- after the language server attaches to the current buffer
 local on_attach = function(client, bufnr)
-    local function buf_set_keymap(...) vim.api.nvim_buf_set_keymap(bufnr, ...) end
-    local function buf_set_option(...) vim.api.nvim_buf_set_option(bufnr, ...) end
-
-    --Enable completion triggered by <c-x><c-o>
-    buf_set_option("omnifunc", "v:lua.vim.lsp.omnifunc")
-
     -- Mappings.
-    local opts = { noremap=true, silent=true }
+    local wk = require("which-key")
 
-    -- See `:help vim.lsp.*` for documentation on any of the below functions
-    buf_set_keymap("n", "gD", "<Cmd>lua vim.lsp.buf.declaration()<CR>", opts)
-    buf_set_keymap("n", "gd", "<Cmd>lua vim.lsp.buf.definition()<CR>", opts)
-    buf_set_keymap("n", "K", "<Cmd>lua vim.lsp.buf.hover()<CR>", opts)
-    buf_set_keymap("n", "gi", "<cmd>lua vim.lsp.buf.implementation()<CR>", opts)
-    buf_set_keymap("n", "<C-k>", "<cmd>lua vim.lsp.buf.signature_help()<CR>", opts)
-    buf_set_keymap("n", "<space>wa", "<cmd>lua vim.lsp.buf.add_workspace_folder()<CR>", opts)
-    buf_set_keymap("n", "<space>wr", "<cmd>lua vim.lsp.buf.remove_workspace_folder()<CR>", opts)
-    buf_set_keymap("n", "<space>wl", "<cmd>lua print(vim.inspect(vim.lsp.buf.list_workspace_folders()))<CR>", opts)
-    buf_set_keymap("n", "<space>D", "<cmd>lua vim.lsp.buf.type_definition()<CR>", opts)
-    buf_set_keymap("n", "<space>rn", "<cmd>lua vim.lsp.buf.rename()<CR>", opts)
-    buf_set_keymap("n", "<space>ca", "<cmd>lua vim.lsp.buf.code_action()<CR>", opts)
-    buf_set_keymap("n", "gr", "<cmd>lua vim.lsp.buf.references()<CR>", opts)
-    buf_set_keymap("n", "<space>e", "<cmd>lua vim.lsp.diagnostic.show_line_diagnostics()<CR>", opts)
-    buf_set_keymap("n", "[d", "<cmd>lua vim.lsp.diagnostic.goto_prev()<CR>", opts)
-    buf_set_keymap("n", "]d", "<cmd>lua vim.lsp.diagnostic.goto_next()<CR>", opts)
-    buf_set_keymap("n", "<space>q", "<cmd>lua vim.lsp.diagnostic.set_loclist()<CR>", opts)
-    buf_set_keymap("n", "<space>f", "<cmd>lua vim.lsp.buf.formatting()<CR>", opts)
+    local mappings = {
+        l = {
+            name = "LSP",
+            g = {
+                name = "Go",
+                d = { function() vim.lsp.buf.definition() end, "Definition" },
+                D = { function() vim.lsp.buf.declaration() end, "Decaration" },
+                y = { function() vim.lsp.buf.type_definition() end, "Type Definition" },
+                r = { function() vim.lsp.buf.references() end, "References" },
+                i = { function() vim.lsp.buf.implementation() end, "Implementation" },
+            },
+            d = {
+                name = "Diagnostics",
+                n = { function() vim.lsp.diagnostic.goto_prev() end, "Next" },
+                p = { function() vim.lsp.diagnostic.goto_next() end, "Previous" },
+                l = { function() vim.lsp.diagnostic.show_line_diagnostics() end, "Line Diagnostics" },
+                q = { function() vim.lsp.diagnostic.set_loclist() end, "Send to loclist"},
+            },
+            r = { function() vim.lsp.buf.rename() end, "Rename" },
+            a = { function() vim.lsp.buf.code_action() end, "Code Action" },
+            f = { function() vim.lsp.buf.formatting() end, "Format" },
+            w = {
+                name = "Workspace",
+                a = { function() vim.lsp.buf.add_workspace_folder() end , "Add" },
+                r = { function() vim.lsp.buf.remove_workspace_folder() end , "Remove" },
+                l = { function() print(vim.inspect(vim.lsp.buf.list_workspace_folders())) end , "List" },
+            },
+        },
+    }
+
+    wk.register(mappings, { prefix = "<leader>" })
+
+    local directMappings = {
+        g = {
+            d = { function() vim.lsp.buf.definition() end, "Definition" },
+            D = { function() vim.lsp.buf.declaration() end, "Decaration" },
+            y = { function() vim.lsp.buf.type_definition() end, "Type Definition" },
+            r = { function() vim.lsp.buf.references() end, "References" },
+            i = { function() vim.lsp.buf.implementation() end, "Implementation" },
+        },
+        K = { function() vim.lsp.buf.hover() end, "Hover" },
+    }
+
+    wk.register(directMappings, { mode = "n", prefix = "" })
+
+    local motionMappings = {
+        ["[d"] = { function() vim.lsp.diagnostic.goto_prev() end, "Previous Diagnostic" },
+        ["]d"] = { function() vim.lsp.diagnostic.goto_prev() end, "Next Diagnostic" },
+    }
+
+    wk.register(motionMappings, { mode = "n", prefix = "" })
+    wk.register(motionMappings, { mode = "o", prefix = "" })
 
     require("rv-lsp/signature").config()
 end
