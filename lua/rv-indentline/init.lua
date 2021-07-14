c726613364b899d2226a32ece7dc553504209ad4
reo101 <pavel.atanasov2001@gmail.com>
2021-07-14 23:23:24 +0300

    Remove duplicate field in indentline's config

--- lua/rv-indentline/init.lua
+++ lua/rv-indentline/init.lua
@@ -4,7 +4,6 @@ M.config = function()
 
     vim.g.indent_blankline_char = "│"
     vim.g.indent_blankline_use_treesitter = true
-    vim.g.indent_blankline_filetype_exclude = { "help" }
     vim.g.indent_blankline_show_current_context = true
     vim.g.indent_blankline_context_patterns = { "class", "function", "method", "while", "do_statement", "closure", "for" }
     vim.g.indent_blankline_viewport_buffer = 50
