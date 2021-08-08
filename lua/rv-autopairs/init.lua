9e23a74d62c9eda52fbeb20507f2f8e28d59a087
reo101 <pavel.atanasov2001@gmail.com>
2021-08-08 22:37:22 +0300

    Add more config to autopairs config...
    Disable java treesitter
    Redo lua string treesitter (with rules)

--- lua/rv-autopairs/init.lua
+++ lua/rv-autopairs/init.lua
@@ -9,7 +9,11 @@ M.config = function()
         enable_afterquote = true,  -- add bracket pairs after quote
         enable_check_bracket_line = true,  --- check bracket in same line
         check_ts = true,
-
+        ts_config = {
+            lua = { "string" }, -- it will not add pair on that treesitter node
+            javascript = { "template_string" },
+            java = false, -- don't check treesitter on java
+        },
     }
 
     local optCompe = {
@@ -18,6 +22,20 @@ M.config = function()
     }
 
     require("nvim-autopairs").setup(opt)
+
+    local autopairs = require("nvim-autopairs")
+    local Rule = require("nvim-autopairs.rule")
+
+    require("nvim-treesitter.configs").setup({ autopairs = { enable = true }})
+
+    local ts_conds = require("nvim-autopairs.ts-conds")
+
+    -- press % => %% is only inside comment or string
+    autopairs.add_rules({
+        Rule("%", "%", "lua"):with_pair(ts_conds.is_ts_node({ "string", "comment" })),
+        Rule("$", "$", "lua"):with_pair(ts_conds.is_not_ts_node({ "function" })),
+    })
+
     require("nvim-autopairs.completion.compe").setup(optCompe)
 
 end
