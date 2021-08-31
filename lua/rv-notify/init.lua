74fe7838cb2e474537710df3a45754344cdf0ae7
reo101 <pavel.atanasov2001@gmail.com>
2021-08-31 16:23:10 +0300

    Add nvim-notify

---
+++ lua/rv-notify/init.lua
@@ -0,0 +1,31 @@
+local M = {}
+
+M.config = function()
+
+    local opt = {
+        -- Animation style (see below for details)
+        stages = "fade_in_slide_out",
+
+        -- Default timeout for notifications
+        timeout = 5000,
+
+        -- For stages that change opacity this is treated as the highlight behind the window
+        background_colour = "Normal",
+
+        -- Icons for the different levels
+        icons = {
+            ERROR = "",
+            WARN = "",
+            INFO = "",
+            DEBUG = "",
+            TRACE = "✎",
+        },
+    }
+
+    require("notify").setup(opt)
+
+    vim.notify = require("notify")
+
+end
+
+return M
