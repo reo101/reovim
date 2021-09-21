ad7c4f9690ce503276c7bc9a2cfd85543ce6fa7a
Pavel Atanasov <pavel.atanasov2001@gmail.com>
2021-09-21 16:55:51 +0300

    Add autocommand for haskell shiftwidth

--- lua/rv-autocommands/init.lua
+++ lua/rv-autocommands/init.lua
@@ -1,3 +1,12 @@
 vim.cmd [[
     autocmd FileType lspinfo nnoremap <silent> <buffer> q :q<CR>
 ]]
+
+local function set_shiftwidth(filetype, shiftwidth)
+    vim.cmd(string.format(
+        [[ autocmd Filetype %s setlocal expandtab tabstop=%d shiftwidth=%d softtabstop=%d ]],
+        filetype, shiftwidth, shiftwidth, shiftwidth
+    ))
+end
+
+set_shiftwidth("haskell", 2)
