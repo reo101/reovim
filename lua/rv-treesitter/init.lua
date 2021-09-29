00a2ddc7688c162c0c1f1d89c29626250b0ae0e6
reo101 <pavel.atanasov2001@gmail.com>
2021-09-29 13:07:53 +0300

    Change ' to " in treesitter config

--- lua/rv-treesitter/init.lua
+++ lua/rv-treesitter/init.lua
@@ -119,21 +119,21 @@ M.config = function()
             updatetime = 25, -- Debounced time for highlighting nodes in the playground from source code
             persist_queries = false, -- Whether the query persists across vim sessions
             keybindings = {
-                toggle_query_editor = 'o',
-                toggle_hl_groups = 'i',
-                toggle_injected_languages = 't',
-                toggle_anonymous_nodes = 'a',
-                toggle_language_display = 'I',
-                focus_language = 'f',
-                unfocus_language = 'F',
-                update = 'R',
-                goto_node = '<cr>',
-                show_help = '?',
+                toggle_query_editor = "o",
+                toggle_hl_groups = "i",
+                toggle_injected_languages = "t",
+                toggle_anonymous_nodes = "a",
+                toggle_language_display = "I",
+                focus_language = "f",
+                unfocus_language = "F",
+                update = "R",
+                goto_node = "<cr>",
+                show_help = "?",
             },
         },
     }
 
-    require('nvim-treesitter.parsers').get_parser_configs().norg = {
+    require("nvim-treesitter.parsers").get_parser_configs().norg = {
         install_info = {
             url = "https://github.com/vhyrro/tree-sitter-norg",
             files = { "src/parser.c" },
