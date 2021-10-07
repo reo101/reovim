382d08f498000e33148c30b76d3e3297a43d6ca9
Pavel Atanasov <pavel.atanasov2001@gmail.com>
2021-10-07 23:21:48 +0300

    Add basic DAP config (no server configurations yet)

---
+++ lua/rv-dap/virttext.lua
@@ -0,0 +1,16 @@
+local M = {}
+
+M.config = function()
+
+    -- virtual text deactivated (default)
+    -- vim.g.dap_virtual_text = false
+
+    -- show virtual text for current frame (recommended)
+    vim.g.dap_virtual_text = true
+
+    -- request variable values for all frames (experimental)
+    -- vim.g.dap_virtual_text = "all frames"
+
+end
+
+return M
