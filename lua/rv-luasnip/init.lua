eb046a763874f612fb49f522047d96fb9abe4c17
reo101 <pavel.atanasov2001@gmail.com>
2021-10-16 01:46:00 +0300

    Move luasnip init to luasnip config file

--- lua/rv-luasnip/init.lua
+++ lua/rv-luasnip/init.lua
@@ -2,23 +2,10 @@ local M = {}
 
 M.config = function()
 
-    local function prequire(...)
-        local status, lib = pcall(require, ...)
-        if (status) then return lib end
-        return nil
-    end
-
-    local luasnip = prequire("luasnip")
-
-    require("luasnip").config.set_config({
+    require("luasnip").config.setup({
         history = false,
-        updateevents = "TextChanged,TextChangedI",
-    })
-
-    require("luasnip.loaders.from_vscode").lazy_load({
-        paths = {
-            vim.fn.stdpath("data") .. "/site/pack/packer/start/friendly-snippets"
-        }
+        updateevents = "InsertLeave,TextChanged,TextChangedI",
+        region_check_events = "CursorMoved,CursorHold,InsertEnter",
     })
 
 end
