e6ac65eced76d5403c74bf0050bd2d55d96e0d75
reo101 <pavel.atanasov2001@gmail.com>
2021-10-19 15:03:22 +0300

    Update luasnip example snippets file

--- lua/rv-luasnip/snippets.lua
+++ lua/rv-luasnip/snippets.lua
@@ -13,6 +13,29 @@ local p = require("luasnip.extras").partial
 local m = require("luasnip.extras").match
 local n = require("luasnip.extras").nonempty
 local dl = require("luasnip.extras").dynamic_lambda
+local fmt = require("luasnip.extras.fmt").fmt
+local fmta = require("luasnip.extras.fmt").fmta
+local types = require("luasnip.util.types")
+local conds = require("luasnip.extras.conditions")
+
+-- Every unspecified option will be set to the default.
+ls.config.set_config({
+	history = true,
+	-- Update more often, :h events for more info.
+	updateevents = "TextChanged,TextChangedI",
+	ext_opts = {
+		[types.choiceNode] = {
+			active = {
+				virt_text = { { "choiceNode", "Comment" } },
+			},
+		},
+	},
+	-- treesitter-hl has 100, use something higher (default is 200).
+	ext_base_prio = 300,
+	-- minimal increase in priority.
+	ext_prio_increase = 1,
+	enable_autosnippets = true,
+})
 
 -- args is a table, where 1 is the text in Placeholder 1, 2 the text in
 -- placeholder 2,...
@@ -33,7 +56,8 @@ rec_ls = function()
 	)
 end
 
-local function jdocsnip(args, old_state)
+-- complicated function for dynamicNode.
+local function jdocsnip(args, _, old_state)
 	local nodes = {
 		t({ "/**", " * " }),
 		i(1, "A short Description"),
@@ -117,7 +141,7 @@ local function jdocsnip(args, old_state)
 end
 
 -- Make sure to not pass an invalid command, as io.popen() may write over nvim-text.
-local function bash(_, command)
+local function bash(_, _, command)
 	local file = io.popen(command, "r")
 	local res = {}
 	for line in file:lines() do
@@ -126,7 +150,21 @@ local function bash(_, command)
 	return res
 end
 
+-- Returns a snippet_node wrapped around an insert_node whose initial
+-- text value is set to the current date in the desired format.
+local date_input = function(args, state, fmt)
+	local fmt = fmt or "%Y-%m-%d"
+	return sn(nil, i(1, os.date(fmt)))
+end
+
 ls.snippets = {
+	-- When trying to expand a snippet, luasnip first searches the tables for
+	-- each filetype specified in 'filetype' followed by 'all'.
+	-- If ie. the filetype is 'lua.c'
+	--     - luasnip.lua
+	--     - luasnip.c
+	--     - luasnip.all
+	-- are searched in that order.
 	all = {
 		-- trigger is fn.
 		s("fn", {
@@ -175,6 +213,14 @@ ls.snippets = {
 			i(0),
 			t({ "", "}" }),
 		}),
+		-- Use a dynamic_node to interpolate the output of a
+		-- function (see date_input above) into the initial
+		-- value of an insert_node.
+		s("novel", {
+			t("It was a dark and stormy night on "),
+			d(1, date_input, {}, "%A, %B %d of %Y"),
+			t(" and the clocks were striking thirteen."),
+		}),
 		-- Parsing snippets: First parameter: Snippet-Trigger, Second: Snippet body.
 		-- Placeholders are parsed into choices with 1. the placeholder text(as a snippet) and 2. an empty string.
 		-- This means they are not SELECTed like in other editors/Snippet engines.
@@ -191,21 +237,42 @@ ls.snippets = {
 
 		-- When regTrig is set, trig is treated like a pattern, this snippet will expand after any number.
 		ls.parser.parse_snippet({ trig = "%d", regTrig = true }, "A Number!!"),
-
+		-- Using the condition, it's possible to allow expansion only in specific cases.
+		s("cond", {
+			t("will only expand in c-style comments"),
+		}, {
+			condition = function(line_to_cursor, matched_trigger, captures)
+				-- optional whitespace followed by //
+				return line_to_cursor:match("%s*//")
+			end,
+		}),
+		-- there's some built-in conditions in "luasnip.extras.conditions".
+		s("cond2", {
+			t("will only expand at the beginning of the line"),
+		}, {
+			condition = conds.line_begin,
+		}),
 		-- The last entry of args passed to the user-function is the surrounding snippet.
 		s(
 			{ trig = "a%d", regTrig = true },
-			f(function(args)
-				return "Triggered with " .. args[1].trigger .. "."
+			f(function(_, snip)
+				return "Triggered with " .. snip.trigger .. "."
 			end, {})
 		),
 		-- It's possible to use capture-groups inside regex-triggers.
 		s(
 			{ trig = "b(%d)", regTrig = true },
-			f(function(args)
-				return "Captured Text: " .. args[1].captures[1] .. "."
+			f(function(_, snip)
+				return "Captured Text: " .. snip.captures[1] .. "."
 			end, {})
 		),
+		s({ trig = "c(%d+)", regTrig = true }, {
+			t("will only expand for even numbers"),
+		}, {
+			condition = function(line_to_cursor, matched_trigger, captures)
+				return tonumber(captures[1]) % 2 == 0
+			end,
+		}),
 		-- Use a function to execute any shell command and print its text.
 		s("bash", f(bash, {}, "ls")),
 		-- Short version for applying String transformations using function nodes.
@@ -223,6 +290,23 @@ ls.snippets = {
 			t({ "", "" }),
 			-- Lambdas can also apply transforms USING the text of other nodes:
 			l(l._1:gsub("e", l._2), { 1, 2 }),
+		}),
+		s({ trig = "trafo(%d+)", regTrig = true }, {
+			-- env-variables and captures can also be used:
+			l(l.CAPTURE1:gsub("1", l.TM_FILENAME), {}),
+		}),
+		-- Set store_selection_keys = "<Tab>" (for example) in your
+		-- luasnip.config.setup() call to access TM_SELECTED_TEXT. In
+		-- this case, select a URL, hit Tab, then expand this snippet.
+		s("link_url", {
+			t('<a href="'),
+			f(function(_, snip)
+				return snip.env.TM_SELECTED_TEXT[1] or {}
+			end, {}),
+			t('">'),
+			i(1),
+			t("</a>"),
+			i(0),
 		}),
 		-- Shorthand for repeating the text in a given node.
 		s("repeat", { i(1, "text"), t({ "", "" }), r(1) }),
@@ -283,12 +367,61 @@ ls.snippets = {
 			t({ "", "" }),
 			dl(3, l._1:gsub("\n", " linebreak ") .. l._2, { 1, 2 }),
 		}),
+		-- Alternative printf-like notation for defining snippets. It uses format
+		-- string with placeholders similar to the ones used with Python's .format().
+		s(
+			"fmt1",
+			fmt("To {title} {} {}.", {
+				i(2, "Name"),
+				i(3, "Surname"),
+				title = c(1, { t("Mr."), t("Ms.") }),
+			})
+		),
+		-- To escape delimiters use double them, e.g. `{}` -> `{{}}`.
+		-- Multi-line format strings by default have empty first/last line removed.
+		-- Indent common to all lines is also removed. Use the third `opts` argument
+		-- to control this behaviour.
+		s(
+			"fmt2",
+			fmt(
+				[[
+			foo({1}, {3}) {{
+				return {2} * {4}
+			}}
+			]],
+				{
+					i(1, "x"),
+					r(1),
+					i(2, "y"),
+					r(2),
+				}
+			)
+		),
+		-- Empty placeholders are numbered automatically starting from 1 or the last
+		-- value of a numbered placeholder. Named placeholders do not affect numbering.
+		s(
+			"fmt3",
+			fmt("{} {a} {} {1} {}", {
+				t("1"),
+				t("2"),
+				a = t("A"),
+			})
+		),
+		-- The delimiters can be changed from the default `{}` to something else.
+		s("fmt4", fmt("foo() { return []; }", i(1, "x"), { delimiters = "[]" })),
+		-- `fmta` is a convenient wrapper that uses `<>` instead of `{}`.
+		s("fmt5", fmta("foo() { return <>; }", i(1, "x"))),
+		-- By default all args must be used. Use strict=false to disable the check
+		s(
+			"fmt6",
+			fmt("use {} only", { t("this"), t("not this") }, { strict = false })
+		),
 	},
 	java = {
 		-- Very long example for a java class.
 		s("fn", {
 			d(6, jdocsnip, { 2, 4, 5 }),
-			t("", ""),
+			t({ "", "" }),
 			c(1, {
 				t("public "),
 				t("private "),
@@ -331,6 +464,20 @@ ls.snippets = {
 	},
 }
 
+-- autotriggered snippets have to be defined in a separate table, luasnip.autosnippets.
+ls.autosnippets = {
+	all = {
+		s("autotrigger", {
+			t("autosnippet"),
+		}),
+	},
+}
+
+-- in a lua file: search lua-, then c-, then all-snippets.
+ls.filetype_extend("lua", { "c" })
+-- in a cpp file: search c-snippets, then all-snippets only (no cpp-snippets!!).
+ls.filetype_set("cpp", { "c" })
+
 --[[
 -- Beside defining your own snippets you can also load snippets from "vscode-like" packages
 -- that expose snippets in json files, for example <https://github.com/rafamadriz/friendly-snippets>.
@@ -338,8 +485,10 @@ ls.snippets = {
 -- will need to extend the table yourself instead of setting a new one.
 ]]
 
-require("luasnip.loaders.from_vscode").load({ include = { "python" } }) -- Load only python snippets
-require("luasnip.loaders.from_vscode").load({ paths = { "./my-snippets" } }) -- Load snippets from my-snippets folder
+require("luasnip/loaders/from_vscode").load({ include = { "python" } }) -- Load only python snippets
+-- The directories will have to be structured like eg. <https://github.com/rafamadriz/friendly-snippets> (include
+-- a similar `package.json`)
+require("luasnip/loaders/from_vscode").load({ paths = { "./my-snippets" } }) -- Load snippets from my-snippets folder
 
 -- You can also use lazy loading so you only get in memory snippets of languages you use
-require("luasnip.loaders.from_vscode").lazy_load() -- You can pass { path = "./my-snippets/"} as well
+require("luasnip/loaders/from_vscode").lazy_load() -- You can pass { paths = "./my-snippets/"} as well
