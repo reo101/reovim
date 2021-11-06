e3ed09d605f6118bfb52ce77b0922aeef7e57290
reo101 <pavel.atanasov2001@gmail.com>
2021-11-06 22:34:02 +0200

    Add "ERROR" to "FIX" alts in todocomments config

--- lua/rv-todocomments/init.lua
+++ lua/rv-todocomments/init.lua
@@ -10,7 +10,7 @@ M.config = function()
             FIX = {
                 icon = " ", -- icon used for the sign, and in search results
                 color = "error", -- can be a hex color, or a named color (see below)
-                alt = { "FIXME", "BUG", "FIXIT", "ISSUE" }, -- a set of other keywords that all map to this FIX keywords
+                alt = { "ERROR", "FIXME", "BUG", "FIXIT", "ISSUE" }, -- a set of other keywords that all map to this FIX keywords
                 -- signs = false, -- configure signs for some keywords individually
             },
             TODO = { icon = " ", color = "info" },
