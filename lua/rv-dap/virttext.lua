418b37001d565b3b648ae5901b860aff44e1e3d3
Pavel Atanasov <pavel.atanasov2001@gmail.com>
2021-11-08 23:17:14 +0200

    Update dap-virttext config to new setup convention

--- lua/rv-dap/virttext.lua
+++ lua/rv-dap/virttext.lua
@@ -6,10 +6,12 @@ M.config = function()
     -- vim.g.dap_virtual_text = false
 
     -- show virtual text for current frame (recommended)
-    vim.g.dap_virtual_text = true
-
+    -- vim.g.dap_virtual_text = true
+    --
     -- request variable values for all frames (experimental)
     -- vim.g.dap_virtual_text = "all frames"
+
+    require("nvim-dap-virtual-text").setup()
 
 end
 
