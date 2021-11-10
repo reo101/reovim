d709c33f924a7cc74aa0d400b20ce256fea714e4
Pavel Atanasov <pavel.atanasov2001@gmail.com>
2021-11-10 04:51:10 +0200

    Add autopairs rule for double spaces in brackets

--- lua/rv-autopairs/init.lua
+++ lua/rv-autopairs/init.lua
@@ -28,6 +28,7 @@ M.config = function()
 
     local autopairs = require("nvim-autopairs")
     local Rule = require("nvim-autopairs.rule")
+    local cond = require'nvim-autopairs.conds'
 
     require("nvim-treesitter.configs").setup({ autopairs = { enable = true }})
 
@@ -38,6 +39,40 @@ M.config = function()
         Rule("%", "%", "lua"):with_pair(ts_conds.is_ts_node({ "string", "comment" })),
         Rule("$", "$", "lua"):with_pair(ts_conds.is_not_ts_node({ "function" })),
     })
+
+
+    autopairs.add_rules {
+        Rule(' ', ' ')
+            :with_pair(function(opts)
+                local pair = opts.line:sub(opts.col -1, opts.col)
+                return vim.tbl_contains({ '()', '{}', '[]' }, pair)
+            end)
+            :with_move(cond.none())
+            :with_cr(cond.none())
+            :with_del(function(opts)
+                local col = vim.api.nvim_win_get_cursor(0)[2]
+                local context = opts.line:sub(col - 1, col + 2)
+                return vim.tbl_contains({ '(  )', '{  }', '[  ]' }, context)
+            end),
+        Rule('', ' )')
+            :with_pair(cond.none())
+            :with_move(function(opts) return opts.char == ')' end)
+            :with_cr(cond.none())
+            :with_del(cond.none())
+            :use_key(')'),
+        Rule('', ' }')
+            :with_pair(cond.none())
+            :with_move(function(opts) return opts.char == '}' end)
+            :with_cr(cond.none())
+            :with_del(cond.none())
+            :use_key('}'),
+        Rule('', ' ]')
+            :with_pair(cond.none())
+            :with_move(function(opts) return opts.char == ']' end)
+            :with_cr(cond.none())
+            :with_del(cond.none())
+            :use_key(']'),
+    }
 
 end
 
