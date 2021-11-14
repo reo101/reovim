6af050a4e3d4e565e698c28050f1755817f8f742
reo101 <pavel.atanasov2001@gmail.com>
2021-11-14 23:30:39 +0200

    Move LSP config run to packages config

--- lua/rv-lsp/init.lua
+++ lua/rv-lsp/init.lua
@@ -1,34 +1,40 @@
-local servers = {
-    clangd = require("rv-lsp.langs.clangd").config,
-    cmake = require("rv-lsp.langs.cmake").config,
-    lua = require("rv-lsp.langs.lua").config,
-    latex = require("rv-lsp.langs.latex").config,
-    null_ls = require("rv-lsp.langs.null-ls").config,
-    python = require("rv-lsp.langs.python").config,
-    sqls = require("rv-lsp.langs.sqls").config,
-    haskell = require("rv-lsp.langs.haskell").config,
-    -- rust = require("rv-lsp.langs.rust").config,
-    go = require("rv-lsp.langs.go").config,
-    tsserver = require("rv-lsp.langs.tsserver").config,
-    java = require("rv-lsp.langs.java").config,
-}
+local M = {}
 
-local function setup_servers()
-    for name, opt in pairs(servers) do
-        if type(opt) == "function" then
-            opt()
-        else
-            local client = require("lspconfig")[name]
-            client.setup(vim.tbl_extend("force", {
-                flags = { debounce_text_changes = 150 },
-                on_attach = require("rv-lsp.utils").lsp_on_attach,
-                on_init = require("rv-lsp.utils").lsp_on_init,
-                capabilities = require("rv-lsp.utils").lsp_capabilities,
-            }, opt))
+M.config = function()
+    local servers = {
+        clangd = require("rv-lsp.langs.clangd").config,
+        cmake = require("rv-lsp.langs.cmake").config,
+        lua = require("rv-lsp.langs.lua").config,
+        latex = require("rv-lsp.langs.latex").config,
+        null_ls = require("rv-lsp.langs.null-ls").config,
+        python = require("rv-lsp.langs.python").config,
+        sqls = require("rv-lsp.langs.sqls").config,
+        haskell = require("rv-lsp.langs.haskell").config,
+        -- rust = require("rv-lsp.langs.rust").config,
+        go = require("rv-lsp.langs.go").config,
+        tsserver = require("rv-lsp.langs.tsserver").config,
+        java = require("rv-lsp.langs.java").config,
+    }
+
+    local function setup_servers()
+        for name, opt in pairs(servers) do
+            if type(opt) == "function" then
+                opt()
+            else
+                local client = require("lspconfig")[name]
+                client.setup(vim.tbl_extend("force", {
+                    flags = { debounce_text_changes = 150 },
+                    on_attach = require("rv-lsp.utils").lsp_on_attach,
+                    on_init = require("rv-lsp.utils").lsp_on_init,
+                    capabilities = require("rv-lsp.utils").lsp_capabilities,
+                }, opt))
+            end
         end
     end
+
+    setup_servers()
+
+    require("rv-lsp.utils").lsp_override_handlers()
 end
 
-setup_servers()
-
-require("rv-lsp.utils").lsp_override_handlers()
+return M
