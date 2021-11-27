668b5bdc3bebc7b0bfd4890d38d86a4c7648cfab
reo101 <pavel.atanasov2001@gmail.com>
2021-11-27 15:06:52 +0200

    Add colourizer config

--- lua/rv-colourizer/init.lua
+++ lua/rv-colourizer/init.lua
@@ -1,10 +1,29 @@
 local M = {}
 
 M.config = function()
-    
-    local opt = {}
 
-    require("colorizer").setup(opt)
+    local opt = {
+        ["css"] = {
+            -- RGB      = true,         -- #RGB hex codes
+            -- RRGGBB   = true,         -- #RRGGBB hex codes
+            -- names    = true,         -- "Name" codes like Blue
+            -- RRGGBBAA = true,         -- #RRGGBBAA hex codes
+            -- rgb_fn   = true,         -- CSS rgb() and rgba() functions
+            -- hsl_fn   = true,         -- CSS hsl() and hsla() functions
+            css      = true,         -- Enable all CSS features: rgb_fn, hsl_fn, names, RGB, RRGGBB
+            -- css_fn   = true,         -- Enable all CSS *functions*: rgb_fn, hsl_fn
+        },
+        "javascript",
+        ["html"] = {
+            mode = "foreground",
+        },
+    }
+
+    local defaults = {
+        mode = "background",
+    }
+
+    require("colorizer").setup(opt, defaults)
 
 end
 
