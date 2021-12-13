c2b4f15101544c86c77b73495822fd7e97aa4811
Pavel Atanasov <pavel.atanasov2001@gmail.com>
2021-12-13 04:21:20 +0200

    Add both cmp-buffer configs (all/visible)

--- lua/rv-cmp/init.lua
+++ lua/rv-cmp/init.lua
@@ -125,11 +125,15 @@ M.config = function()
                 name = "buffer",
                 option = {
                     get_bufnrs = function()
+                        -- Visible Buffers
                         local bufs = {}
                         for _, win in ipairs(vim.api.nvim_list_wins()) do
                             bufs[vim.api.nvim_win_get_buf(win)] = true
                         end
                         return vim.tbl_keys(bufs)
+
+                        -- All Buffers
+                        -- return vim.api.nvim_list_bufs()
                     end,
                 },
             },
