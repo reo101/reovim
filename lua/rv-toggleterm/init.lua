f6ae4355c9a984b813bc98eba146635199c5d55a
Pavel Atanasov <pavel.atanasov2001@gmail.com>
2021-12-13 04:19:32 +0200

    Change default toggleterm orientation to vertical

--- lua/rv-toggleterm/init.lua
+++ lua/rv-toggleterm/init.lua
@@ -19,8 +19,8 @@ M.config = function()
         start_in_insert = true,
         insert_mappings = true, -- whether or not the open mapping applies in insert mode
         persist_size = true,
-        -- direction = "vertical" | "horizontal" | "window" | "float"
-        direction = "float",
+        -- direction = "vertical" | "horizontal" | "tab" | "float",
+        direction = "vertical",
         close_on_exit = true, -- close the terminal window when the process exits
         shell = vim.o.shell, -- change the default shell
         -- This field is only relevant if direction is set to "float"
