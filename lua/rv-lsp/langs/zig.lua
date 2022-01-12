176daf517223507c6c9deb78b405941c6e396fa0
Pavel Atanasov <pavel.atanasov2001@gmail.com>
2022-01-12 23:12:36 +0200

    Add zls for Zig

---
+++ lua/rv-lsp/langs/zig.lua
@@ -0,0 +1,25 @@
+local M = {}
+
+M.config = function()
+
+    local opt = {
+        cmd = { "zls" },
+        filetypes = { "zig", "zir" },
+        on_attach = require("rv-lsp.utils").lsp_on_attach,
+        on_init = require("rv-lsp.utils").lsp_on_init,
+        capabilities = require("rv-lsp.utils").capabilities,
+        root_dir = function(fname)
+            local util = require("lspconfig.util")
+            local root_files = {
+                "zls.json",
+            }
+            return util.root_pattern(unpack(root_files))(fname) or util.find_git_ancestor(fname) or util.path.dirname(fname)
+        end,
+        single_file_support = true,
+    }
+
+    require("lspconfig")["zls"].setup(opt)
+
+end
+
+return M
