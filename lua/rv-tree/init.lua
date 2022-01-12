7eba050467590c9ec6e80751fccb57eb210b652c
reo101 <pavel.atanasov2001@gmail.com>
2022-01-12 23:08:06 +0200

    Turn off auto-resize for nvim-tree

--- lua/rv-tree/init.lua
+++ lua/rv-tree/init.lua
@@ -56,7 +56,7 @@ M.config = function()
             -- side of the tree, can be one of "left" | "right" | "top" | "bottom"
             side = "right",
             -- if true the tree will resize itself after opening a file
-            auto_resize = true,
+            auto_resize = false,
             mappings = {
                 -- custom only false will merge the list with the default mappings
                 -- if true, it will only use your list to set the mappings
