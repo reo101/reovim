77b5d9c57bd183b96e8cad75e028acdb7b7ddda1
reo101 <pavel.atanasov2001@gmail.com>
2022-01-12 23:05:36 +0200

    Add aerial to telescope extensions

--- lua/rv-telescope/init.lua
+++ lua/rv-telescope/init.lua
@@ -97,6 +97,7 @@ M.config = function()
     require("telescope").load_extension("gh")
     require("telescope").load_extension("media_files")
     require("telescope").load_extension("notify")
+    require("telescope").load_extension("aerial")
     require("telescope").load_extension("file_browser")
 
     local actions = require("telescope.actions")
