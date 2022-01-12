4f0bc217665bfbdc73499df31b297763a6ecdc7d
reo101 <pavel.atanasov2001@gmail.com>
2022-01-12 23:10:14 +0200

    Temporarily disable two sources for null-ls ...
    shellcheck and cppcheck

--- lua/rv-lsp/langs/null-ls.lua
+++ lua/rv-lsp/langs/null-ls.lua
@@ -5,12 +5,12 @@ M.config = function()
     local opt = {
         sources = {
             require("null-ls").builtins.formatting.stylua,
-            require("null-ls").builtins.formatting.shellcheck,
+            -- require("null-ls").builtins.formatting.shellcheck,
             require("null-ls").builtins.formatting.eslint_d,
             require("null-ls").builtins.formatting.prettierd,
             require("null-ls").builtins.formatting.fixjson,
 
-            require("null-ls").builtins.diagnostics.cppcheck,
+            -- require("null-ls").builtins.diagnostics.cppcheck,
         },
         on_attach = require("rv-lsp.utils").lsp_on_attach,
         on_init = require("rv-lsp.utils").lsp_on_init,
