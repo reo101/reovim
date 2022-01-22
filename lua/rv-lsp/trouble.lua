30967761b193ef31c25867fbe30c36d29b47e448
Pavel Atanasov <pavel.atanasov2001@gmail.com>
2022-01-22 18:11:15 +0200

    Update Trouble config (`<leader>tr` toggle)

--- lua/rv-lsp/trouble.lua
+++ lua/rv-lsp/trouble.lua
@@ -7,9 +7,11 @@ M.config = function()
             height = 10, -- height of the trouble list when position is top or bottom
             width = 50, -- width of the list when position is left or right
             icons = true, -- use devicons for filenames
-            mode = "lsp_workspace_diagnostics", -- "lsp_workspace_diagnostics", "lsp_document_diagnostics", "quickfix", "lsp_references", "loclist"
+            mode = "workspace_diagnostics", -- "workspace_diagnostics", "document_diagnostics", "quickfix", "lsp_references", "loclist"
             fold_open = "", -- icon used for open folds
             fold_closed = "", -- icon used for closed folds
+            group = true, -- group results by file
+            padding = true, -- add an extra new line on top of the list
             action_keys = { -- key mappings for actions in the trouble list
                 -- map to {} to remove a mapping, for example:
                 -- close = {},
@@ -36,6 +38,7 @@ M.config = function()
             auto_close = false, -- automatically close the list when you have no diagnostics
             auto_preview = true, -- automatyically preview the location of the diagnostic. <esc> to close preview and go back to last window
             auto_fold = false, -- automatically fold a file trouble list at creation
+            auto_jump = {"lsp_definitions"}, -- for the given modes, automatically jump if there is only a single result
             signs = {
                 -- icons / text used for a diagnostic
                 error = "",
@@ -44,11 +47,21 @@ M.config = function()
                 information = "",
                 other = "﫠"
             },
-            use_lsp_diagnostic_signs = false -- enabling this will use the signs defined in your lsp client
+            use_diagnostic_signs = false -- enabling this will use the signs defined in your lsp client
     }
 
     require("trouble").setup(opt)
 
+    local wk = require("which-key")
+
+    local mappings = {
+        t = {
+            name = "Toggle",
+            r = { require("trouble").toggle, "Trouble" },
+        },
+    }
+
+    wk.register(mappings, { prefix = "<leader>" })
 end
 
 return M
