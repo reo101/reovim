28f52c9b356506d126a450f335bba45b4d08a67f
reo101 <pavel.atanasov2001@gmail.com>
2022-01-24 14:35:20 +0200

    Update comments config...
    <leader>c - line-comment (motion)
    <leader>cc - toggle line-comment
    
    <leader>C - block-comment (motion)
    <leader>Cc - toggle block-comment
    
    Warning: toggles dont work when quickly pressing only the first two
    buttons (<leader> and c/C). Only motions work in this case (WK bug)

--- lua/rv-comments/init.lua
+++ lua/rv-comments/init.lua
@@ -7,25 +7,16 @@ M.config = function()
         ---@type boolean
         padding = true,
 
+        ---Whether the cursor should stay at its position
+        ---NOTE: This only affects NORMAL mode mappings and doesn't work with dot-repeat
+        ---@type boolean
+        sticky = true,
+
         ---Lines to be ignored while comment/uncomment.
         ---Could be a regex string or a function that returns a regex string.
         ---Example: Use "^$" to ignore empty lines
         ---@type string|function
         ignore = nil,
-
-        ---Create basic (operator-pending) and extended mappings for NORMAL + VISUAL mode
-        ---@type table
-        mappings = {
-            ---operator-pending mapping
-            ---Includes `<leader>cc`, `<leader>cb`, `<leader>cc[count]{motion}` and `<leader>cb[count]{motion}`
-            basic = true,
-            ---extra mapping
-            -- HACK: Done using which-key
-            extra = false,
-            ---extended mapping
-            -- TODO: Do using which-key
-            extended = false,
-        },
 
         ---LHS of toggle mapping in NORMAL + VISUAL mode
         ---@type table
@@ -33,16 +24,50 @@ M.config = function()
             ---line-comment keymap
             line = "<leader>cc",
             ---block-comment keymap
-            block = "<leader>cb",
+            block = "<leader>Cc",
         },
 
         ---LHS of operator-pending mapping in NORMAL + VISUAL mode
         ---@type table
         opleader = {
             ---line-comment keymap
-            line = "<leader>cC",
+            line = "<leader>c",
             ---block-comment keymap
-            block = "<leader>cB",
+            block = "<leader>C",
+        },
+
+        ---LHS of extra mappings
+        ---@type table
+        extra = {
+            ---Add comment on the line above
+            above = '<leader>cO',
+            ---Add comment on the line below
+            below = '<leader>co',
+            ---Add comment at the end of line
+            eol = '<leader>cA',
+        },
+
+        --LHS of extended mappings
+        ---@type table
+        extended = {
+            ---Add comment on the line above
+            above = '<leader>cO',
+            ---Add comment on the line below
+            below = '<leader>co',
+            ---Add comment at the end of line
+            eol = '<leader>cA',
+        },
+
+        ---Create basic (operator-pending) and extended mappings for NORMAL + VISUAL mode
+        ---@type table
+        mappings = {
+            ---operator-pending mapping
+            ---Includes `<leader>cc`, `<leader>cb` and `<leader>c[count]{motion}`
+            basic = true,
+            ---extra mapping
+            extra = true,
+            ---extended mapping
+            extended = false,
         },
 
         ---Pre-hook, called before commenting the line
@@ -62,25 +87,31 @@ M.config = function()
 
     require("Comment").setup(opt);
 
+    local comment_api = require("Comment.api")
+
     local wk = require("which-key")
 
-    local E = require("Comment.extra")
-    local U = require("Comment.utils")
-
     local mappings = {
-        c = {
-            name = "Comment",
-            c = "Toggle Line",
-            C = "Toggle Line Op",
-            b = "Toggle Block",
-            B = "Toggle Block Op",
-            o = { function() E.norm_o(U.ctype.line, opt) end, "o" },
-            O = { function() E.norm_O(U.ctype.line, opt) end, "O" },
-            A = { function() E.norm_A(U.ctype.line, opt) end, "A" },
+        ["c"] = {
+            name = "Line Comment",
+            c = { comment_api.toggle_current_linewise_op, "Toggle Line" },
+            o = { comment_api.insert_linewise_below, "o" },
+            O = { comment_api.insert_linewise_above, "O" },
+            A = { comment_api.insert_linewise_eol, "A" },
+        },
+        ["C"] = {
+            name = "Block Comment",
+            c = { comment_api.toggle_current_blockwise_op, "Toggle line" },
         },
     }
 
+    local operatorMappings = {
+        ["c"] = { comment_api.toggle_linewise_op },
+        ["C"] = { comment_api.toggle_linewise_op },
+    }
+
     wk.register(mappings, { prefix = "<leader>" })
+    wk.register(operatorMappings, { mode = "o", prefix = "<leader>" })
 
 end
 
