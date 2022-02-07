d06e3ebff6c8d5f9f0554106074a616d1dcae18e
reo101 <pavel.atanasov2001@gmail.com>
2022-02-07 23:11:20 +0200

    Refactor neorg config...
    Change to new keybinding style
    Disable default keybind (TODO all custom)
    Enable presenter mode (no keybinds yet)

--- lua/rv-neorg/init.lua
+++ lua/rv-neorg/init.lua
@@ -8,8 +8,30 @@ M.config = function()
             ["core.defaults"] = {}, -- Load all the default modules
             ["core.keybinds"] = { -- Configure core.keybinds
                 config = {
-                    default_keybinds = true, -- Generate the default keybinds
+                    default_keybinds = false, -- Generate the default keybinds
                     neorg_leader = "<leader>o", -- This is the default if unspecified
+
+                    hook = function(keybinds)
+                        local mappings = {}
+
+                        mappings["gt"] = { name = "Task" }
+
+                        keybinds.map("norg", "n", "gtd", "core.norg.qol.todo_items.task_done")
+                        mappings["gtd"] = { "Done" }
+
+                        keybinds.map("norg", "n", "gtu", "core.norg.qol.todo_items.task_undone")
+                        mappings["gtu"] = { "Undone" }
+
+                        keybinds.map("norg", "n", "gtp", "core.norg.qol.todo_items.task_pending")
+                        mappings["gtp"] = { "Pending" }
+
+                        keybinds.map("norg", "n", "<C-Space>", "core.norg.qol.todo_items.todo.task_cycle")
+                        mappings["<C-Space>"] = { "Cycle Task" }
+
+                        local wk = require("which-key")
+
+                        wk.register(mappings, { prefix = "" })
+                    end,
                 },
             },
             ["core.norg.concealer"] = {}, -- Allows for use of icons
@@ -30,46 +52,13 @@ M.config = function()
                 config = {
                     type = "auto",
                 }
-            },
+            }, -- Enable the metagen module
+            ["core.presenter"] = {
+                config = {
+                    zen_mode = "truezen",
+                }
+            }, -- Enable the presenter module
         },
-        hook = function()
-
-            local neorg_callbacks = require("neorg.callbacks")
-
-            local wk = require("which-key")
-
-            local mappings = {
-                o = {
-                    name = "Neorg",
-                },
-            }
-
-            wk.register(mappings, { prefix = "<leader>" })
-
-            neorg_callbacks.on_event("core.keybinds.events.enable_keybinds", function(_, keybinds)
-
-                keybinds.map_event_to_mode("norg", {
-                    n = {
-                        { "gtd", "core.norg.qol.todo_items.todo.task_done" },
-                        { "gtu", "core.norg.qol.todo_items.todo.task_undone" },
-                        { "gtp", "core.norg.qol.todo_items.todo.task_pending" },
-                        { "<C-Space>", "core.norg.qol.todo_items.todo.task_cycle" }
-                    },
-                }, { silent = true, noremap = true })
-
-                wk.register({
-                    ["gt"] = {
-                        name = "Task",
-                        d = { "Done" },
-                        u = { "Undone" },
-                        p = { "Pending" },
-                    },
-                    ["<C-Space>"] = { "Cycle Task" },
-                }, { silent = true, noremap = true })
-
-            end)
-
-        end,
     }
 
     require("neorg").setup(opt)
