cdaf716e47b7f95ecd64dd25b1176ec75e43aba6
Pavel Atanasov <pavel.atanasov2001@gmail.com>
2022-02-19 18:18:44 +0200

    Add another injection for Regex in Java...
    {String,Pattern}.{match,matches,compile}("regex here")

--- queries/java/injections.scm
+++ queries/java/injections.scm
@@ -1,3 +1,8 @@
 ((string_literal) @regex
   (#lua-match? @regex "^\"\^.*\$\"$")
   (#offset! @regex 0 1 0 -1))
+
+(method_invocation
+  object: (identifier) @_object (#any-of? @_object "String" "Pattern")
+  name: (identifier) @_name (#any-of? @_name "match" "matches" "compile")
+  arguments: (argument_list (string_literal) @regex))
