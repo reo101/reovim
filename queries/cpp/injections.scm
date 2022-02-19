b901ea4bcf6743a0a598b66a3063b6eb27b1594d
Pavel Atanasov <pavel.atanasov2001@gmail.com>
2022-02-19 18:17:05 +0200

    Unify indentation in all TS queries

--- queries/cpp/injections.scm
+++ queries/cpp/injections.scm
@@ -1,5 +1,3 @@
-(
-    (raw_string_literal) @glsl
-    (#lua-match? @glsl "^R\"%(%s*#version %d%d%d")
-    (#offset! @glsl 0 3 0 -2)
-)
+((raw_string_literal) @glsl
+  (#lua-match? @glsl "^R\"%(%s*#version %d%d%d")
+  (#offset! @glsl 0 3 0 -2))
--- queries/java/injections.scm
+++ queries/java/injections.scm
@@ -1,5 +1,3 @@
-(
-    (string_literal) @regex
-    (#lua-match? @regex "^\"\^.*\$\"$")
-    (#offset! @regex 0 1 0 -1)
-)
+((string_literal) @regex
+  (#lua-match? @regex "^\"\^.*\$\"$")
+  (#offset! @regex 0 1 0 -1))
