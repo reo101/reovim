250bf158c11d18b61668944fc8e9fe142b6eb088
Pavel Atanasov <pavel.atanasov2001@gmail.com>
2022-02-19 18:24:38 +0200

    Fix and revert some changes for some queries...
    Add offset for the new Regex for Java injection
    Make queries with *external* predicates use the old indentation style

--- queries/rust/injections.scm
+++ queries/rust/injections.scm
@@ -1,11 +1,17 @@
-((string_literal) @glsl
+(
+  (string_literal) @glsl
   (#lua-match? @glsl "^\"%s*#version %d%d%d")
-  (#offset! @glsl 0 1 0 -1))
+  (#offset! @glsl 0 1 0 -1)
+)
 
-((raw_string_literal) @glsl
+(
+  (raw_string_literal) @glsl
   (#lua-match? @glsl "^r\"%s*#version %d%d%d")
-  (#offset! @glsl 0 2 0 -2))
+  (#offset! @glsl 0 2 0 -2)
+)
 
-((raw_string_literal) @glsl
+(
+  (raw_string_literal) @glsl
   (#lua-match? @glsl "^r#\"%s*#version %d%d%d")
-  (#offset! @glsl 0 3 0 -2))
+  (#offset! @glsl 0 3 0 -2)
+)
