3bbb07176ab25a21a5fa6d409e8a43948202d1fb
reo101 <pavel.atanasov2001@gmail.com>
2022-02-19 18:24:38 +0200

    Fix and revert some changes for some queries...
    Add offset for the new Regex for Java injection
    Make queries with *external* predicates use the old indentation style

--- queries/cpp/injections.scm
+++ queries/cpp/injections.scm
@@ -1,3 +1,5 @@
-((raw_string_literal) @glsl
+(
+  (raw_string_literal) @glsl
   (#lua-match? @glsl "^R\"%(%s*#version %d%d%d")
-  (#offset! @glsl 0 3 0 -2))
+  (#offset! @glsl 0 3 0 -2)
+)
--- queries/java/injections.scm
+++ queries/java/injections.scm
@@ -1,8 +1,15 @@
-((string_literal) @regex
+(
+  (string_literal) @regex
   (#lua-match? @regex "^\"\^.*\$\"$")
-  (#offset! @regex 0 1 0 -1))
+  (#offset! @regex 0 1 0 -1)
+)
 
-(method_invocation
-  object: (identifier) @_object (#any-of? @_object "String" "Pattern")
-  name: (identifier) @_name (#any-of? @_name "match" "matches" "compile")
-  arguments: (argument_list (string_literal) @regex))
+(
+  (method_invocation
+    object: (identifier) @_object
+    name: (identifier) @_name
+    arguments: (argument_list (string_literal) @regex))
+  (#any-of? @_object "String" "Pattern")
+  (#any-of? @_name "match" "matches" "compile")
+  (#offset! @regex 0 1 0 -1)
+)
