3beaf2df67f69635d46b18653f9cfe525df0936e
Pavel Atanasov <pavel.atanasov2001@gmail.com>
2022-02-23 22:12:36 +0200

    Swap out nvim-tree for neo-tree

--- lua/rv-tree/init.lua
+++ lua/rv-tree/init.lua
@@ -2,155 +2,167 @@ local M = {}
 
 M.config = function()
 
+    -- See ":help neo-tree-highlights" for a list of available highlight groups
+    vim.cmd([[
+        hi link NeoTreeDirectoryName Directory
+        hi link NeoTreeDirectoryIcon NeoTreeDirectoryName
+    ]])
+
     local opt = {
-        -- disables netrw completely
-        disable_netrw       = true,
-        -- hijack netrw window on startup
-        hijack_netrw        = true,
-        -- open the tree when running this setup function
-        open_on_setup       = false,
-        -- will not open on setup if the filetype is in this list
-        ignore_ft_on_setup  = {},
-        -- closes neovim automatically when the tree is the last **WINDOW** in the view
-        auto_close          = true,
-        -- opens the tree when changing/opening a new tab if the tree wasn't previously opened
-        open_on_tab         = false,
-        -- hijack the cursor in the tree to put it at the start of the filename
-        hijack_cursor       = true,
-        -- updates the root directory of the tree on `DirChanged` (when your run `:cd` usually)
-        update_cwd          = true,
-        -- show lsp diagnostics in the signcolumn
-        diagnostics = {
-            -- enables the feature
-            enable = true,
-            -- icons for diagnostic severity
-            icons = {
-                ["hint"] = "",
-                ["info"] = "",
-                ["warning"] = "",
-                ["error"] = "",
+        close_if_last_window = false, -- Close Neo-tree if it is the last window left in the tab
+        popup_border_style = "rounded",
+        enable_git_status = true,
+        enable_diagnostics = true,
+        default_component_configs = {
+            indent = {
+                indent_size = 2,
+                padding = 1, -- extra padding on left hand side
+                with_markers = true,
+                indent_marker = "│",
+                last_indent_marker = "└",
+                highlight = "NeoTreeIndentMarker",
+            },
+            icon = {
+                folder_closed = "",
+                folder_open = "",
+                folder_empty = "ﰊ",
+                default = "*",
+            },
+            name = {
+                trailing_slash = true,
+                use_git_status_colors = true,
+            },
+            git_status = {
+                highlight = "NeoTreeDimText", -- if you remove this the status will be colorful
             },
         },
-        -- update the focused file on `BufEnter`, un-collapses the folders recursively until it finds the file
-        update_focused_file = {
-            -- enables the feature
-            enable      = true,
-            -- update the root directory of the tree to the one of the folder containing the file if the file is not under the current root directory
-            -- only relevant when `update_focused_file.enable` is true
-            update_cwd  = false,
-            -- list of buffer names / filetypes that will not update the cwd if the file isn't found under the current root directory
-            -- only relevant when `update_focused_file.update_cwd` is true and `update_focused_file.enable` is true
-            ignore_list = {},
+        filesystem = {
+            filters = { --These filters are applied to both browsing and searching
+                show_hidden = true,
+                respect_gitignore = true,
+            },
+            follow_current_file = true, -- This will find and focus the file in the active buffer every
+            -- time the current file is changed while the tree is open.
+            use_libuv_file_watcher = true, -- This will use the OS level file watchers
+            -- to detect changes instead of relying on nvim autocmd events.
+            hijack_netrw_behavior = "open_split",
+            -- "open_default", -- netrw disabled, opening a directory opens neo-tree
+            -- in whatever position is specified in window.position
+            -- "open_split",  -- netrw disabled, opening a directory opens within the
+            -- window like netrw would, regardless of window.position
+            -- "disabled",    -- netrw left alone, neo-tree does not handle opening dirs
+            window = {
+                position = "left",
+                width = 30,
+                mappings = {
+                    ["<2-LeftMouse>"] = "open",
+                    ["<CR>"]  = "open",
+                    ["S"]     = "open_split",
+                    ["s"]     = "open_vsplit",
+                    ["C"]     = "close_node",
+                    ["<BS>"]  = "navigate_up",
+                    ["."]     = "set_root",
+                    ["H"]     = "toggle_hidden",
+                    ["I"]     = "toggle_gitignore",
+                    ["R"]     = "refresh",
+                    -- ["/"]     = "fuzzy_finder",
+                    ["/"]     = "filter_on_submit",
+                    -- ["/"]   = "filter_as_you_type", -- this was the default until v1.28
+                    --["/"]   = "none" -- Assigning a key to "none" will remove the default mapping
+                    ["f"]     = "filter_on_submit",
+                    ["<C-X>"] = "clear_filter",
+                    ["<C-W>"] = "clear_filter",
+                    ["a"]     = "add",
+                    ["d"]     = "delete",
+                    ["r"]     = "rename",
+                    ["c"]     = "copy_to_clipboard",
+                    ["x"]     = "cut_to_clipboard",
+                    ["p"]     = "paste_from_clipboard",
+                    ["m"]     = "move", -- takes text input for destination
+                    ["q"]     = "close_window",
+                },
+            },
         },
-        -- configuration options for the system open command (`s` in the tree by default)
-        system_open = {
-            -- the command to run this, leaving nil should work in most cases
-            cmd  = nil,
-            -- the command arguments as a list
-            args = {},
+        buffers = {
+            show_unloaded = true,
+            window = {
+                position = "left",
+                mappings = {
+                    ["<2-LeftMouse>"] = "open",
+                    ["<CR>"] = "open",
+                    ["S"]    = "open_split",
+                    ["s"]    = "open_vsplit",
+                    ["<BS>"] = "navigate_up",
+                    ["."]    = "set_root",
+                    ["R"]    = "refresh",
+                    ["a"]    = "add",
+                    ["d"]    = "delete",
+                    ["r"]    = "rename",
+                    ["c"]    = "copy_to_clipboard",
+                    ["x"]    = "cut_to_clipboard",
+                    ["p"]    = "paste_from_clipboard",
+                    ["bd"]   = "buffer_delete",
+                },
+            },
         },
-
-        view = {
-            -- width of the window, can be either a number (columns) or a string in `%`
-            width = 30,
-            -- side of the tree, can be one of "left" | "right" | "top" | "bottom"
-            side = "right",
-            -- if true the tree will resize itself after opening a file
-            auto_resize = false,
-            mappings = {
-                -- custom only false will merge the list with the default mappings
-                -- if true, it will only use your list to set the mappings
-                custom_only = false,
-                -- list of mappings to set on the tree manually
-                list = {},
+        git_status = {
+            window = {
+                position = "float",
+                mappings = {
+                    ["<2-LeftMouse>"] = "open",
+                    ["<CR>"] = "open",
+                    ["S"]    = "open_split",
+                    ["s"]    = "open_vsplit",
+                    ["C"]    = "close_node",
+                    ["R"]    = "refresh",
+                    ["d"]    = "delete",
+                    ["r"]    = "rename",
+                    ["c"]    = "copy_to_clipboard",
+                    ["x"]    = "cut_to_clipboard",
+                    ["p"]    = "paste_from_clipboard",
+                    ["A"]    = "git_add_all",
+                    ["gu"]   = "git_unstage_file",
+                    ["ga"]   = "git_add_file",
+                    ["gr"]   = "git_revert_file",
+                    ["gc"]   = "git_commit",
+                    ["gp"]   = "git_push",
+                    ["gg"]   = "git_commit_and_push",
+                },
             },
         },
     }
 
-    --  Dictionary of buffer option names mapped to a list of option values that
-    --  indicates to the window picker that the buffer"s window should not be
-    --  selectable.
-    vim.g.nvim_tree_window_picker_exclude = {
-        ["filetype"] = {
-            "packer",
-            "qf",
-        },
-        ["buftype"] = {
-            "terminal",
-        },
-    }
-
-    --  List of filenames that gets highlighted with NvimTreeSpecialFile
-    vim.g.nvim_tree_special_files = {
-        ["README.md"] = 1,
-        ["Makefile"] = 1,
-        ["MAKEFILE"] = 1,
-    }
-
-    -- 0 by default, this option shows indent markers when folders are open
-    vim.g.nvim_tree_indent_markers = 1
-
-    -- 0 by default, compact folders that only contain a single folder into one node in the file tree
-    vim.g.nvim_tree_group_empty = 1
-
-    -- If 0, do not show the icons for one of "git" "folder" and "files"
-    -- 1 by default, notice that if "files" is 1, it will only display
-    -- if nvim-web-devicons is installed and on your runtimepath.
-    -- if folder is 1, you can also tell folder_arrows 1 to show small arrows next to the folder icons.
-    -- but this will not work when you set indent_markers (because of UI conflict)
-    vim.g.nvim_tree_show_icons = {
-        ["git"] = 1,
-        ["folders"] = 1,
-        ["files"] = 1,
-        ["folder_arrows"] = 1,
-    }
-
-    --  default will show icon by default if no icon is provided
-    --  default shows no icon by default
-    vim.g.nvim_tree_icons = {
-        ["default"] = "",
-        ["symlink"] = "",
-        ["git"] = {
-            ["unstaged"] = "✗",
-            ["staged"] = "✓",
-            ["unmerged"] = "",
-            ["renamed"] = "➜",
-            ["untracked"] = "★",
-            ["deleted"] = "",
-            ["ignored"] = "◌",
-        },
-        ["folder"] = {
-            ["arrow_open"] = "",
-            ["arrow_closed"] = "",
-            ["default"] = "",
-            ["open"] = "",
-            ["empty"] = "",
-            ["empty_open"] = "",
-            ["symlink"] = "",
-            ["symlink_open"] = "",
-        },
-    }
-
-    require("nvim-tree").setup(opt)
+    require("neo-tree").setup(opt)
 
     local wk = require("which-key")
+
+    -- command! -nargs=? NeoTreeClose         lua require("neo-tree").close_all("<args>")
+    -- command! -nargs=? NeoTreeFloat         lua require("neo-tree").float("<args>")
+    -- command! -nargs=? NeoTreeFocus         lua require("neo-tree").focus("<args>")
+    -- command! -nargs=? NeoTreeShow          lua require("neo-tree").show("<args>", true)
+    -- command! -bang    NeoTreeReveal        lua require("neo-tree").reveal_current_file("filesystem", false, "<bang>" == "!")
+    -- command!          NeoTreeRevealInSplit lua require("neo-tree").reveal_in_split("filesystem", false)
+    -- command!          NeoTreeShowInSplit   lua require("neo-tree").show_in_split("filesystem", false)
+
+    -- command! -nargs=? NeoTreeFloatToggle   lua require("neo-tree").float("<args>", true)
+    -- command! -nargs=? NeoTreeFocusToggle   lua require("neo-tree").focus("<args>", true, true)
+    -- command! -nargs=? NeoTreeShowToggle    lua require("neo-tree").show("<args>", true, true, true)
+    -- command! -bang    NeoTreeRevealToggle  lua require("neo-tree").reveal_current_file("filesystem", true, "<bang>" == "!")
+    -- command!    NeoTreeRevealInSplitToggle lua require("neo-tree").reveal_in_split("filesystem", true)
+    -- command!    NeoTreeShowInSplitToggle   lua require("neo-tree").show_in_split("filesystem", true)
+
+    -- command!          NeoTreePasteConfig   lua require("neo-tree").paste_default_config()
+    -- command! -nargs=? NeoTreeSetLogLevel   lua require("neo-tree").set_log_level("<args>")
+    -- command!          NeoTreeLogs          lua require("neo-tree").show_logs()
 
     local mappings = {
         t = {
             name = "Toggle",
-            f = { require("nvim-tree").toggle, "FileTree" },
+            f = { function() require("neo-tree").reveal_current_file("filesystem", true, false) end, "FileTree" },
         },
     }
 
     wk.register(mappings, { prefix = "<leader>" })
-
-    --nnoremap <C-n> :NvimTreeToggle<CR>
-    --nnoremap <leader>r :NvimTreeRefresh<CR>
-    --nnoremap <leader>n :NvimTreeFindFile<CR>
-    --  NvimTreeOpen and NvimTreeClose are also available if you need them
-
-    --  a list of groups can be found at `:help nvim_tree_highlight`
-    --highlight NvimTreeFolderIcon guibg=blue
 
 end
 
