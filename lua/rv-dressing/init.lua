1f8d602a23cc8db66a1f6ffa5f9e48ef5198d919
Pavel Atanasov <pavel.atanasov2001@gmail.com>
2022-03-03 15:36:14 +0200

    Update dressing config (new config defaults)

--- lua/rv-dressing/init.lua
+++ lua/rv-dressing/init.lua
@@ -1,9 +1,11 @@
 local M = {}
 
 M.config = function()
-
     local opt = {
         input = {
+            -- Set to false to disable the vim.ui.input implementation
+            enabled = true,
+
             -- Default prompt string
             default_prompt = "➤ ",
 
@@ -12,37 +14,58 @@ M.config = function()
 
             -- These are passed to nvim_open_win
             anchor = "SW",
-            relative = "cursor",
-            row = 0,
-            col = -2,
             border = "rounded",
+            -- 'editor' and 'win' will default to being centered
+            relative = "cursor",
 
             -- These can be integers or a float between 0 and 1 (e.g. 0.4 for 40%)
             prefer_width = 40,
-            max_width = 0.6,
-            min_width = 20,
+            width = nil,
+            -- min_width and max_width can be a list of mixed types.
+            -- min_width = {20, 0.2} means "the greater of 20 columns or 20% of total"
+            max_width = { 140, 0.9 },
+            min_width = { 20, 0.2 },
 
             -- Window transparency (0-100)
-            winblend = 20,
+            winblend = 10,
             -- Change default highlight groups (see :help winhl)
             winhighlight = "",
+
+            override = function(conf)
+                -- This is the config that will be passed to nvim_open_win.
+                -- Change values here to customize the layout
+                return conf
+            end,
 
             -- see :help dressing_get_config
             get_config = nil,
         },
         select = {
+            -- Set to false to disable the vim.ui.select implementation
+            enabled = true,
+
             -- Priority list of preferred vim.select implementations
-            backend = { "telescope", "fzf", "builtin", "nui" },
+            backend = { "telescope", "fzf_lua", "fzf", "builtin", "nui" },
 
             -- Options for telescope selector
             telescope = {
                 -- can be 'dropdown', 'cursor', or 'ivy'
+                -- or you can use a configuration directly:
+                -- theme = require('telescope.themes').get_ivy({...})
                 theme = "cursor",
             },
 
             -- Options for fzf selector
             fzf = {
                 window = {
+                    width = 0.5,
+                    height = 0.4,
+                },
+            },
+
+            -- Options for fzf_lua selector
+            fzf_lua = {
+                winopts = {
                     width = 0.5,
                     height = 0.4,
                 },
@@ -64,10 +87,9 @@ M.config = function()
             builtin = {
                 -- These are passed to nvim_open_win
                 anchor = "NW",
-                relative = "cursor",
-                row = 0,
-                col = 0,
                 border = "rounded",
+                -- 'editor' and 'win' will default to being centered
+                relative = "editor",
 
                 -- Window transparency (0-100)
                 winblend = 10,
@@ -75,12 +97,20 @@ M.config = function()
                 winhighlight = "",
 
                 -- These can be integers or a float between 0 and 1 (e.g. 0.4 for 40%)
+                -- the min_ and max_ options can be a list of mixed types.
+                -- max_width = {140, 0.8} means "the lesser of 140 columns or 80% of total"
                 width = nil,
-                max_width = 0.8,
-                min_width = 40,
+                max_width = { 140, 0.8 },
+                min_width = { 40, 0.2 },
                 height = nil,
                 max_height = 0.9,
-                min_height = 10,
+                min_height = { 10, 0.2 },
+
+                override = function(conf)
+                    -- This is the config that will be passed to nvim_open_win.
+                    -- Change values here to customize the layout
+                    return conf
+                end,
             },
 
             -- Used to override format_item. See :help dressing-format
@@ -103,7 +133,6 @@ M.config = function()
     }
 
     require("dressing").setup(opt)
-
 end
 
 return M
