e112d17af88f6431ffc0992c954583bea8d0a805
Pavel Atanasov <pavel.atanasov2001@gmail.com>
2022-03-21 23:38:17 +0200

    Refactor luasnip config...
    Snippets are now autodetected at luasnippets/
    Add keybind (`<leader>tl` to edit luasnip filetype snippets)
    Add cpp snippets

--- lua/rv-luasnip/init.lua
+++ lua/rv-luasnip/init.lua
@@ -1,7 +1,6 @@
 local M = {}
 
 M.config = function()
-
     local opt = {
         history = false,
         updateevents = "InsertLeave,TextChanged,TextChangedI",
@@ -9,61 +8,34 @@ M.config = function()
         ext_opts = {
             [require("luasnip.util.types").choiceNode] = {
                 active = {
-                    virt_text = {{"●", "DiagnosticError"}}
-                }
+                    virt_text = { { "●", "DiagnosticError" } },
+                },
             },
             [require("luasnip.util.types").insertNode] = {
                 active = {
-                    virt_text = {{"●", "DiagnosticInfo"}}
-                }
-            }
+                    virt_text = { { "●", "DiagnosticInfo" } },
+                },
+            },
         },
     }
 
     require("luasnip").config.setup(opt)
     require("luasnip.loaders.from_vscode").lazy_load()
+    require("luasnip.loaders.from_lua").lazy_load()
 
-    local luasnip = require("luasnip")
+    local wk = require("which-key")
 
-    function _G.snippets_clear()
-        for m, _ in pairs(luasnip.snippets) do
-            package.loaded["snippets."..m] = nil
-        end
-        luasnip.snippets = setmetatable({}, {
-            __index = function(t, k)
-                local ok, m = pcall(require, "snippets." .. k)
-                if not ok and not string.match(m, "module.*not found:") then
-                    error(m)
-                end
-                t[k] = ok and m or {}
-                return t[k]
-            end
-        })
-    end
+    local mappings = {
+        t = {
+            name = "Toggle",
+            l = {
+                require("luasnip.loaders.from_lua").edit_snippet_files,
+                "LuaSnip snippets",
+            },
+        },
+    }
 
-    _G.snippets_clear()
-
-    vim.cmd [[
-    augroup snippets_clear
-    au!
-    au BufWritePost ~/.config/nvim/lua/snippets/*.lua lua _G.snippets_clear()
-    augroup END
-    ]]
-
-    function _G.edit_ft()
-        -- returns table like {"lua", "all"}
-        local fts = require("luasnip.util.util").get_snippet_filetypes()
-        vim.ui.select(fts, {
-            prompt = "Select which filetype to edit:"
-        }, function(item, idx)
-            -- selection aborted -> idx == nil
-            if idx then
-                vim.cmd("edit ~/.config/nvim/lua/snippets/" .. item .. ".lua")
-            end
-        end)
-    end
-
-    vim.cmd [[command! LuaSnipEdit :lua _G.edit_ft()]]
+    wk.register(mappings, { prefix = "<leader>" })
 end
 
 return M
