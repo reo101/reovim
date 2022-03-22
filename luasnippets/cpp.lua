58727d8974427564864a630207c115bbffda1918
Pavel Atanasov <pavel.atanasov2001@gmail.com>
2022-03-22 02:08:50 +0200

    Refactor cpp snippets...
    Replace node chains with the `fmt` helper
    Fully* implement the std::function macro
    
    * doesn't (yet) work with array types (as the param name must be before
      the `[]`) nor with function pointers (similar reason)

--- luasnippets/cpp.lua
+++ luasnippets/cpp.lua
@@ -19,96 +19,98 @@ local fmta = require("luasnip.extras.fmt").fmta
 local types = require("luasnip.util.types")
 local conds = require("luasnip.extras.expand_conditions")
 
-local function params(args, snip, old_state, initial_text)
-    local nodes = {}
-	if not old_state then
-        old_state = {}
-    end
-
-	-- -- count is nil for invalid input.
-	-- local count = tonumber(args[1][1])
-	-- -- Make sure there's a number in args[1].
-	-- if count then
-	-- 	for j=1, count do
-	-- 		local iNode
-	-- 		if old_state and old_state[j] then
-	-- 			-- old_text is used internally to determine whether
-	-- 			-- dependents should be updated. It is updated whenever the
-	-- 			-- node is left, but remains valid when the node is no
-	-- 			-- longer 'rendered', whereas node:get_text() grabs the text
-	-- 			-- directly from the node.
-	-- 			iNode = i(j, old_state[j].old_text)
-	-- 		else
-	-- 		  iNode = i(j, initial_text)
-	-- 		end
-	-- 		nodes[2*j-1] = iNode
-	--
-	-- 		-- linebreak
-	-- 		nodes[2*j] = t({"",""})
-	-- 		-- Store insertNode in old_state, potentially overwriting older
-	-- 		-- nodes.
-	-- 		old_state[j] = iNode
-	-- 	end
-	-- else
-	-- 	nodes[1] = t("Enter a number!")
-	-- end
-
-    print(vim.inspect(args))
-
+local return_type = function(args)
     if args[1][1] == "void" then
-        nodes[1] = t(" ")
-        nodes[2] = i(1, "param_name")
+        return ""
     else
-        nodes[1] = t("")
+        return " -> " .. args[1][1] .. " "
     end
 
-	local new_snip = sn(nil, nodes)
-	new_snip.old_state = old_state
-	return new_snip
+    return sn(nil, i(1, os.date(fmt)))
+end
+
+local params = function(args, snip, old_state, ...)
+    local nodes = {}
+
+    if args[1][1] == "" or args[1][1] == "void" then
+        table.insert(nodes, t(""))
+    else
+        -- TODO: do this with TS
+        local index = 1
+
+        for type in (args[1][1] .. ","):gmatch("(.-)%s*,%s*") do
+            P(type)
+            table.insert(nodes, t(type .. " "))
+            -- table.insert(nodes, i(index, type .. "_param"))
+            table.insert(nodes, i(index, "param" .. index))
+            table.insert(nodes, t(", "))
+            index = index + 1
+        end
+
+        -- Remove trailing comma
+        table.remove(nodes, #nodes)
+    end
+
+    return sn(nil, nodes)
 end
 
 local snippets = {
+    s(
+        "func",
+        fmt(
+            [[
+            std::function<{}({})> {} = [{}]({}){}{{
+                {}
+            }};
+            ]],
+            {
+                -- return type
+                i(1, "void"),
+                -- param types
+                i(2, "int"),
+                -- name
+                i(3, "func"),
+                -- captures
+                c(4, {
+                    t(""),
+                    t("&"),
+                    t("="),
+                }),
+                -- params
+                d(5, params, { 2 }),
+                -- return type
+                f(return_type, { 1 }),
+                -- body
+                i(0),
+            }
+        )
+    ),
+
     -- faster HackerRank IO
-    s("magic", {
-        t({ "std::ios::sync_with_stdio(false);",
-            "std::cin.tie(nullptr);" }),
-    }),
+    s(
+        "magic",
+        fmt(
+            [[
+            std::ios::sync_with_stdio(false);
+            std::cin.tie(nullptr);
+            ]],
+            {}
+        )
+    ),
 
-    s("main", {
-        t({ "int main() {", "    " }),
-        i(0),
-        t({ "", "", "    return 0;", "}" }),
-    }),
+    s(
+        "main",
+        fmt(
+            [[
+            int main() {{
+                {}
 
-    s("stdfunction", {
-        t("std::function<"),
-        i(1, "void"),
-        t("("),
-        i(2, "int"),
-        t(")> "),
-        i(3, "func"),
-        t(" = ["),
-        c(4, {
-            t(""),
-            t("&"),
-            t("="),
-        }),
-        t("]("),
-        rep(2),
-        d(5, params, { 2 }),
-        -- c(5, {
-        --     t(nil),
-        --     sn(nil, {
-        --         t(" "),
-        --         i(1, ""),
-        --     }),
-        -- }),
-        t(") -> "),
-        rep(1),
-        t({ " {", "\t"}),
-        i(0),
-        t({ "", "};" }),
-    }),
+                return 0;
+            }}
+            ]],
+            { i(0) }
+        )
+    ),
 }
 
 return snippets
