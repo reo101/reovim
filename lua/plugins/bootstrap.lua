4c11b693f0fba0914c17615c9cbdff82cf6a1df2
Pavel Atanasov <pavel.atanasov2001@gmail.com>
2022-03-29 02:26:19 +0300

    Add HotPot (beginning of fennel config)

--- lua/plugins/bootstrap.lua
+++ lua/plugins/bootstrap.lua
@@ -1,10 +1,35 @@
-local execute = vim.api.nvim_command
-local fn = vim.fn
+-- Packer
 
-local install_path = fn.stdpath("data").."/site/pack/packer/start/packer.nvim"
+local packer_path = vim.fn.stdpath("data")
+    .. "/site/pack/packer/start/packer.nvim"
 
-if fn.empty(fn.glob(install_path)) > 0 then
-  fn.system({"git", "clone", "https://github.com/wbthomason/packer.nvim", install_path})
-  execute("packadd packer.nvim")
-  execute("packadd vimball")
+if vim.fn.empty(vim.fn.glob(packer_path)) > 0 then
+    print("Could not find packer.nvim, cloning new copy to ", packer_path)
+    vim.fn.system({
+        "git",
+        "clone",
+        "https://github.com/wbthomason/packer.nvim",
+        packer_path,
+    })
+    vim.cmd("packadd packer.nvim")
+    vim.cmd("packadd vimball")
 end
+
+-- HotPot
+
+local hotpot_path = vim.fn.stdpath("data")
+    .. "/site/pack/packer/start/hotpot.nvim"
+
+if vim.fn.empty(vim.fn.glob(hotpot_path)) > 0 then
+    print("Could not find hotpot.nvim, cloning new copy to ", hotpot_path)
+    vim.fn.system({
+        "git",
+        "clone",
+        "https://github.com/rktjmp/hotpot.nvim",
+        hotpot_path,
+    })
+    vim.cmd("helptags " .. hotpot_path .. "/doc")
+end
+
+require("hotpot")
+require("fenneled_init")
