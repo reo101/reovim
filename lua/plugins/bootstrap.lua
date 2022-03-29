9aef8cd22591cce99ff71010dfb038675d7b41b2
Pavel Atanasov <pavel.atanasov2001@gmail.com>
2022-03-29 18:10:30 +0300

    Refactor bootstrap

--- lua/plugins/bootstrap.lua
+++ lua/plugins/bootstrap.lua
@@ -1,35 +1,47 @@
--- Packer
-
-local packer_path = vim.fn.stdpath("data")
-    .. "/site/pack/packer/start/packer.nvim"
-
-if vim.fn.empty(vim.fn.glob(packer_path)) > 0 then
-    print("Could not find packer.nvim, cloning new copy to ", packer_path)
-    vim.fn.system({
-        "git",
-        "clone",
-        "https://github.com/wbthomason/packer.nvim",
-        packer_path,
-    })
-    vim.cmd("packadd packer.nvim")
-    vim.cmd("packadd vimball")
+---Install essential plugins without a package manager
+---@param user string
+---@param repo string
+---@param additional_cmd fun(install_path: string)
+local function ensure(user, repo, additional_cmd)
+    local install_path = string.format(
+        "%s/packer/start/%s",
+        vim.fn.stdpath("data") .. "/site/pack",
+        repo
+    )
+    if vim.fn.empty(vim.fn.glob(install_path)) > 0 then
+        print(
+            string.format(
+                "Could not find %s, cloning new copy to %s",
+                repo,
+                install_path
+            )
+        )
+        vim.fn.system({
+            "git",
+            "clone",
+            string.format(
+                "https://www.github.com/%s/%s %s",
+                user,
+                repo,
+                install_path
+            ),
+        })
+        vim.cmd(string.format("packadd %s", repo))
+        if additional_cmd then
+            additional_cmd(install_path)
+        end
+    end
 end
+
+-- Packer
+ensure("wbthomason", "packer.nvim", function()
+    vim.cmd("packadd vimball")
+end)
 
 -- HotPot
-
-local hotpot_path = vim.fn.stdpath("data")
-    .. "/site/pack/packer/start/hotpot.nvim"
-
-if vim.fn.empty(vim.fn.glob(hotpot_path)) > 0 then
-    print("Could not find hotpot.nvim, cloning new copy to ", hotpot_path)
-    vim.fn.system({
-        "git",
-        "clone",
-        "https://github.com/rktjmp/hotpot.nvim",
-        hotpot_path,
-    })
+ensure("rktjmp", "hotpot.nvim", function(hotpot_path)
     vim.cmd("helptags " .. hotpot_path .. "/doc")
-end
+end)
 
 require("hotpot")
 require("fenneled_init")
