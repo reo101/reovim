f4eab26d3d96c55b2854ddc35156974ef7efbc1a
reo101 <pavel.atanasov2001@gmail.com>
2022-04-22 01:59:11 +0300

    Refactor fennel's treesitter highlight queries...
    Now properly highlights _VARARG and all occurrences of `fn` and `lambda`

--- queries/fennel/highlights.scm
+++ queries/fennel/highlights.scm
@@ -1,11 +1,29 @@
-(
-  ("lambda") @keyword.function @conceal
-  (#set! conceal "λ")
-)
-(
-  ("λ") @keyword.function
-)
-(
-  ("fn") @keyword.function @conceal
-  (#set! conceal "")
-)
+;; _VARARG
+((symbol) @constant.builtin
+ (#any-of? @constant.builtin
+  "_VARARG")
+ (#set! priority 150))
+
+;; Lambdas
+(["lambda" "λ"] @keyword.function @conceal
+  (#set! conceal "λ"))
+
+((symbol) @keyword.function @conceal
+ (#any-of? @keyword.function
+  "lambda" "λ")
+ (#set! conceal "λ"))
+
+;; Functions
+(("fn") @keyword.function @conceal
+  (#set! conceal ""))
+
+((symbol) @keyword.function @conceal
+ (#any-of? @keyword.function
+  "fn")
+ (#set! conceal ""))
+
+;; Hash Functions
+((symbol) @keyword.function @conceal
+ (#any-of? @keyword.function
+  "hashfn" "#")
+ (#set! conceal "#"))
