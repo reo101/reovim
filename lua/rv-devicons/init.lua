588d3ae6dcbd6ad2126bb7c6874808d5cd864405
Pavel Atanasov <pavel.atanasov2001@gmail.com>
2022-04-22 01:57:29 +0300

    Add a devicon for Fennel (🌱)

--- lua/rv-devicons/init.lua
+++ lua/rv-devicons/init.lua
@@ -6,11 +6,16 @@ M.config = function()
         -- your personnal icons can go here (to override)
         -- DevIcon will be appended to `name`
         override = {
+            fnl = {
+                icon = "🌱",
+                color = "#428850",
+                name = "Fennel",
+            },
             zsh = {
                 icon = "",
                 color = "#428850",
                 name = "Zsh"
-            }
+            },
         };
         -- globally enable default icons (default to false)
         -- will get overriden by `get_icons` option
