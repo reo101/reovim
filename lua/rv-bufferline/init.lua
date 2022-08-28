ce04c971115321609f22785d294a8e168ed10ea1
reo101 <pavel.atanasov2001@gmail.com>
2022-08-28 21:01:23 +0300

    fix(bufferline): update `indicator_icon` option
    `indicator_icon` -> `indicator.icon`

--- lua/rv-bufferline/init.lua
+++ lua/rv-bufferline/init.lua
@@ -18,7 +18,9 @@ M.config = function()
             -- NOTE: this plugin is designed with this icon in mind,
             -- and so changing this is NOT recommended, this is intended
             -- as an escape hatch for people who cannot bear it for whatever reason
-            indicator_icon = "▎",
+            indicator = {
+                icon = "▎",
+            },
             buffer_close_icon = "",
             modified_icon = "●",
             close_icon = "",
