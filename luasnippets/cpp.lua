96903aea5d025e06c5930c8fd15862a9417e5428
reo101 <pavel.atanasov2001@gmail.com>
2022-10-10 08:11:00 +0300

    fix(luasnippets): update api

--- luasnippets/cpp.lua
+++ luasnippets/cpp.lua
@@ -17,7 +17,7 @@ local dl = require("luasnip.extras").dynamic_lambda
 local fmt = require("luasnip.extras.fmt").fmt
 local fmta = require("luasnip.extras.fmt").fmta
 local types = require("luasnip.util.types")
-local conds = require("luasnip.extras.expand_conditions")
+local conds = require("luasnip.extras.conditions.expand")
 
 local return_type = function(args)
     if args[1][1] == "void" then
