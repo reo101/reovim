2c620c182c14ee6dfd36ddb2137200bc9ab30607
reo101 <pavel.atanasov2001@gmail.com>
2024-02-20 13:53:31 +0200

    fix(queries): `rust` injections
    Upgrade to new format (forgot to stage for big commit doing that)
    Also add `sol_interface!` solidity injection (for Arbitrum)

--- after/queries/rust/injections.scm
+++ after/queries/rust/injections.scm
@@ -1,19 +1,38 @@
 ;; extends
 
 (
-  (string_literal) @glsl
-  (#lua-match? @glsl "^\"%s*#version %d%d%d")
-  (#offset! @glsl 0 1 0 -1)
+  (string_literal) @injection.content
+  (#lua-match? @injection.content "^\"%s*#version %d%d%d")
+  (#offset! @injection.content 0 1 0 -1)
+  (#set! injection.language "glsl")
 )
 
 (
-  (raw_string_literal) @glsl
-  (#lua-match? @glsl "^r\"%s*#version %d%d%d")
-  (#offset! @glsl 0 2 0 -2)
+  (raw_string_literal) @injection.content
+  (#lua-match? @injection.content "^r\"%s*#version %d%d%d")
+  (#offset! @injection.content 0 2 0 -2)
+  (#set! injection.language "glsl")
 )
 
 (
-  (raw_string_literal) @glsl
-  (#lua-match? @glsl "^r#\"%s*#version %d%d%d")
-  (#offset! @glsl 0 3 0 -2)
+  (raw_string_literal) @injection.content
+  (#lua-match? @injection.content "^r#\"%s*#version %d%d%d")
+  (#offset! @injection.content 0 3 0 -2)
+  (#set! injection.language "glsl")
+)
+
+(
+  (macro_invocation
+    macro:
+      [
+        (scoped_identifier
+          name: (_) @_macro_name)
+        (identifier) @_macro_name
+      ]
+    (token_tree) @injection.content)
+  (#eq? @_macro_name "sol_interface")
+  (#offset! @injection.content 0 1 0 -1)
+  (#set! injection.language "solidity")
+  (#set! injection.include-children)
+  (#set! "priority" 128)
 )
