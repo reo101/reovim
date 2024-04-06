423802f63bf50e0701a6fa57af5e764b05709869
reo101 <pavel.atanasov2001@gmail.com>
2024-04-06 18:02:14 +0300

    fix(nix)!: disable highlight queries
    Breaks highlighting for some reason, TODO fix

--- after/queries/nix/highlights.scm
+++ after/queries/nix/highlights.scm
@@ -6,22 +6,22 @@
 ;;     (#set! conceal "\$")
 ;; )
 
-(
-    (
-      (dollar_escape) @_dollar_escape
-    ) @string.escape.nix
-    (#set! conceal "")
-)
-
-(
-    (
-      (string_fragment) @_dollar
-    ) @string.escape.nix
-    (#lua-match? @_dollar "^%$$")
-)
-
-(
-    (escape_sequence) @string.escape.nix
-    (#lua-match? @string.escape.nix "^%'%'\\.$")
-    (#setgsub! conceal @string.escape.nix "^%'%'\\(.)$" "%1")
-)
+; (
+;     (
+;       (dollar_escape) @_dollar_escape
+;     ) @string.escape.nix
+;     (#set! conceal "")
+; )
+;
+; (
+;     (
+;       (string_fragment) @_dollar
+;     ) @string.escape.nix
+;     (#lua-match? @_dollar "^%$$")
+; )
+;
+; (
+;     (escape_sequence) @string.escape.nix
+;     (#lua-match? @string.escape.nix "^%'%'\\.$")
+;     (#setgsub! conceal @string.escape.nix "^%'%'\\(.)$" "%1")
+; )
