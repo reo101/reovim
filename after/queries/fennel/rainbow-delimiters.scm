9d5aae15b044ea23aba3c400eb84c5879e612745
reo101 <pavel.atanasov2001@gmail.com>
2025-07-06 16:10:54 +0300

    fix(rainbow): `fennel` query

--- after/queries/fennel/rainbow-delimiters.scm
+++ after/queries/fennel/rainbow-delimiters.scm
@@ -5,11 +5,11 @@
     (symbol) @_colon @delimiter
   value:
     (symbol) @_value @delimiter
-  (#eq? @_colon ":"))
+  (#eq? @_colon ":")) @container
 
 (table_pair
   key:
     (string
       content: _ @delimiter)
   value:
-    _ @_value)
+    _ @_value) @container
